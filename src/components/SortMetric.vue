b9b1384d59ed9f8182cd7615a143048d5bac57e7
Mystic4l <127591572+Mystic4l@users.noreply.github.com>
2023-05-19 15:44:45 +0800

    Update SortMetric.vue
    Change echart to display horizontally

--- src/components/SortMetric.vue
+++ src/components/SortMetric.vue
@@ -15,6 +15,8 @@ export default {
   },
   mounted () {
     this.mychart = this.$echarts.init(this.$refs.sort_metric)
+    console.log(this.sort_metric_param[1])
+    console.log(this.sort_metric_param[0])
   },
   watch: {
     sort_metric_param: {
@@ -28,8 +30,8 @@ export default {
   },
   computed: {
     options() {
-      let x_data = this.sort_metric_param[0]
-      let y_data = this.sort_metric_param[1]
+      let x_data = this.sort_metric_param[0].reverse()
+      let y_data = this.sort_metric_param[1].reverse()
 
       return {
         legend : {
@@ -38,17 +40,17 @@ export default {
           textStyle:{color:'#fff'}
         },
         xAxis: {
-          type: 'category',
-          data: x_data,
+          type: 'value',
           axisLabel: {
             color: '#fff'
           }
         },
         yAxis: {
-          type: 'value',
+          type: 'category',
           axisLabel: {
             color: '#fff'
-          }
+          },
+          data: x_data,
         },
         tooltip: {
           show: true,
@@ -93,7 +95,7 @@ export default {
             yAxisIndex: 0,
             handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
             handleSize: '80%',
-            width: 20,
+            width: 40,
             handleStyle: {
               color: '#fff',
               shadowBlur: 3,
