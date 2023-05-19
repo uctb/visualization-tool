abc5c24b5582c55c364c63bfc1f9212d9e18d436
Mystic4l <127591572+Mystic4l@users.noreply.github.com>
2023-05-19 19:56:46 +0800

    Update TimeInfoProcessor.js
    Update TimeInfoProcessor.js

--- src/TimeInfoProcessor.js
+++ src/TimeInfoProcessor.js
@@ -2,16 +2,26 @@ export default class TimeInfoProcessor
 {
     /*专门处理时间有关的信息 */
     constructor() {
-
+        this.start_time = ""
+        this.end_time = ""
+        this.time_fitness = 0
+        this.time_type = ""
     }
-    updateParam(start_date,end_date,time_fitness){
-        this.start_date = start_date
-        this.end_date = end_date
+    updateParam(start_time,end_time,time_fitness,time_type){
+        this.start_time = start_time
+        this.end_time = end_time
         this.time_fitness = time_fitness
+        this.time_type = time_type
+    }
+    refresh(){
+        this.start_time = ""
+        this.end_time = ""
+        this.time_fitness = 0
+        this.time_type = ""
     }
     emitTimeSeries() {
         console.log("here")
-        console.log(typeof this.start_date)
+        console.log("start",this)
         // let start_year = this.start_date.split(" ")[0]
         // console.log(start_year)
     }
