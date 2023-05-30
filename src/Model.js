01c0af6bac54ea70b1558abf87357ed382239347
Mystic4l <127591572+Mystic4l@users.noreply.github.com>
2023-05-30 10:30:23 +0800

    fix refresh bug
    fix refresh bug

--- src/Model.js
+++ src/Model.js
@@ -147,6 +147,7 @@ export default class Model {
         this.mae_distribution_param = {};
         this.sort_rmse_param = {};
         this.sort_mae_param = {};
+        this.ts_flag = false;
     }
 
     // update(jsonData) {
