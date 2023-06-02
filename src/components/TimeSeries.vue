41cfa406e2d7fc732071f50f1374af83f1e0f5e7
Mystic4l <127591572+Mystic4l@users.noreply.github.com>
2023-06-02 22:20:41 +0800

    修改界面
    修改界面

--- src/components/TimeSeries.vue
+++ src/components/TimeSeries.vue
@@ -11,10 +11,10 @@
   </el-date-picker>
   <div class="flex center_spaceBetweeen">
     <ul class="Datasets">
-         <el-input v-model="interval" size="mini" id="datasets" placeholder="Input Time Fitness/min"/>
+      <el-input v-model="interval" size="mini" id="datasets" placeholder="Input Time Fitness/min" style="margin-left: .7rem;"/>
     </ul>
     <div>
-      <el-button  type="warning" size="mini" id="el-button" @click="updateTime">setting<i class="el-icon-edit el-icon--right"></i></el-button>
+      <el-button  type="warning" size="mini" id="el-button" @click="updateTime" style="margin-right: .7rem;">setting<i class="el-icon-edit el-icon--right"></i></el-button>
       <el-icon><EditPen /></el-icon>
     </div>
   </div>
@@ -62,7 +62,7 @@ export default {
 	width: 10%;
 	height: .5rem;
 	text-align: left;
-	margin-left: .2rem;
+	margin-right: .1rem;
 }
 <<<.el-button{
 	width: .1rem;
